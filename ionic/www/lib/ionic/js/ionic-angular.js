318138986eb8b7f69478ad8942cda73b7a2d2647
Shindeok Kang <heavymery@gmail.com>
2015-06-18 09:07:19 +0900

    Update ionic lib

--- ionic/www/lib/ionic/js/ionic-angular.js
+++ ionic/www/lib/ionic/js/ionic-angular.js
@@ -11061,7 +11061,7 @@ IonicModule
  * @usage
  *
  * ```html
- * <body ng-app="angularFirechat">
+ * <body ng-app="starter">
  *   <!-- The nav bar that will be updated as we navigate -->
  *   <ion-nav-bar class="bar-positive">
  *   </ion-nav-bar>
